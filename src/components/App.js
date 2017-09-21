2ee4fb05bc942bfa8ad382e71051286199bb047a
Nick Foden <nickfoden@gmail.com>
2017-09-20 21:19:06 -0400

    Build and link add to order button functionality passing key as index

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
 
         this.addFish = this.addFish.bind(this);
         this.loadSamples = this.loadSamples.bind(this);
+        this.addToOrder = this.addToOrder.bind(this);
 
         this.state = {
             fishes: {},
@@ -31,6 +32,15 @@ class App extends React.Component {
         })
     }
 
+    addToOrder(key){
+        //take copy of state
+        const order = {...this.state.order};
+        //update or add the new number of fish
+        order[key] = order[key] + 1 || 1;
+        // then update state 
+        this.setState({order});
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -40,8 +50,8 @@ class App extends React.Component {
                         {
                             Object
                             .keys(this.state.fishes)
-                            .map(key => <Fish key={key} details={this.state.fishes[key]}
-                             />)
+                            .map(key => <Fish key={key} index={key} details={this.state.fishes[key]}
+                             addToOrder={this.addToOrder} />)
                         }
                     </ul>    
                 </div>
